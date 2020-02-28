b58c8927938f48229a895074986fcaa6b8780b7c
orlando-yerter <53948381+orlando-yerter@users.noreply.github.com>
2020-02-28 15:58:22 -0300

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,11 +1,11 @@
 from tkinter import *
-import modulos
+#import modulos
 
 #funcion inserta elemento en un caja de texto
 def listar(fin):
     agenda = open("agendatelefonica.csv")
     numero = 0
-    for i in range(1,fin):
+    for i in range(0,fin):
         lectura = agenda.readline()
         #print(lectura.replace(",","\t\t"))
         lista.insert(END,lectura.replace(",","  "))
@@ -29,13 +29,16 @@ autor = Label(f,text="Developer: Juan Manuel Chaguendo")
 autor.pack(side=TOP,padx=10,pady=10)
 
 #botones y eventos(command mas lambda permite pasar parametros al metodo de otro modulo)
-#Entry.get() obtiene el dato de un input
-'''
-btn1 = Button(f,text="Listar",command=lambda:modulos.listar(int(Entry.get(campo))))
-btn1.pack(side=BOTTOM,padx=10,pady=10)
-'''
+#Entry.get() obtiene el dato de un input,   buton 2 de salir  28/2/20 x guille
+
+btn2 = Button(f,text="salir",command=quit)
+btn2.pack(side=BOTTOM,padx=10,pady=10)
+
 btn1 = Button(f,text="Listar Elementos",command=lambda:listar(int(Entry.get(campo))))
 btn1.pack(side=BOTTOM,padx=10,pady=10)
+
+
+
 
 #campos de Entrada
 campo = Entry(f,textvariable=5)
